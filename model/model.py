d704a5b2b578b6ee0d3a8dbf2430659df8a25b78
周宇峰 <369282275@qq.com>
2025-07-24 00:34:17 +0800

    删除多余接口

--- model/model.py
+++ model/model.py
@@ -20,18 +20,6 @@ class Model:
         )
         return response.choices[0].message.content
 
-    def api_call_tools(self, system_prompt: str, prompt: str, tools: list, tool_choice: str = "auto"):
-        response = self.client.chat.completions.create(
-            model=self.model_name,
-            messages=[
-                {"role": "system", "content": system_prompt},
-                {"role": "user", "content": prompt}
-            ],
-            tools=tools,
-            tool_choice=tool_choice
-        )
-        return response.choices[0].message
-
 class OllamaModel:
     def __init__(self, config: SearchConfig):
         self.model_name = config.ollama_model_name
